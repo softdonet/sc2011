453adba71f29dba25b7f9422d3e9b4160889994f
yanghongkang <yanghongkang@gmail.com>
2011-10-06 13:40:23 +0000

    添加中文地图

---
+++ 03.需求分析/SCADA_Demo/SCADA.UI/Modules/BingMaps/ChinaMode.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.Core;
+using Microsoft.Maps.MapControl;
+
+namespace SCADA.UI.Modules.BingMaps
+{
+    /// <summary>
+    /// 自定义地图投影模式
+    /// </summary>
+    public class ChinaMode : MercatorMode
+    {
+        // 地图坐标取值范围最小（-85.05112877980659,-180）最大（85.05112877980659,180）
+        // 中国范围大致是：LocationRect(new Location(60, 60), new Location(13, 140))
+
+        ///　<summary>
+        ///　纬度范围
+        ///　</summary>
+        public Range<double> LatitudeRange = new Range<double>(-85.05112877980659, -180);
+        ///　<summary>
+        ///　经度范围
+        ///　</summary>
+        public Range<double> LongitudeRange = new Range<double>(85.05112877980659, 180);
+        ///　<summary>
+        ///　深度缩放范围
+        ///　</summary>
+        public Range<double> MapZoomRange = new Range<double>(1.0, 20.0);
+
+        /// <summary>
+        /// 获取中文地图图层
+        /// </summary>
+        public MapTileLayer GetChinaTileLayer()
+        {
+            //中文地图瓦片
+            MapTileLayer tileChinaLayer = new MapTileLayer();
+            //初始化一个Uri对象，指向中文必应地图的Tile系统
+            UriBuilder tileSourceUri = new UriBuilder("http://r2.tiles.ditu.live.com/tiles/r{quadkey}.png?g=41");
+            LocationRectTileSource tileSource = new LocationRectTileSource(
+            tileSourceUri.Uri.ToString(), new LocationRect(new Location(LatitudeRange.From, LatitudeRange.To), new Location(LongitudeRange.From, LongitudeRange.To)),
+             new Range<double>(MapZoomRange.From, MapZoomRange.To));
+            tileChinaLayer.TileSources.Add(tileSource); //指定图层的TileSource
+            tileChinaLayer.Opacity = 1;
+            return tileChinaLayer;
+        }
+
+        ///　<summary>
+        ///　重写方法
+        ///　</summary>
+        ///　<param　name="center">地图中心地理坐标</param>
+        ///　<param　name="zoomLevel">地图缩放级别</param>
+        ///　<param　name="heading">定向视图标题</param>
+        ///　<param　name="pitch"></param>
+        ///　<returns>是否有值更改</returns>
+        public override bool ConstrainView(Location center, ref　double zoomLevel, ref　double heading, ref　double pitch)
+        {
+            return base.ConstrainView(center, ref　zoomLevel, ref　heading, ref　pitch);
+        }
+        ///　<summary>
+        ///　确定地图深度缩放范围
+        ///　</summary>
+        ///　<param　name="center"></param>
+        ///　<returns></returns>
+        protected override Range<double> GetZoomRange(Location center)
+        {
+            //只能访问3-10级地图
+            return this.MapZoomRange;
+        }
+        ///　<summary>
+        ///　重写鼠标中键滚轮操作行为
+        ///　</summary>
+        ///　<param　name="e"></param>
+        public override void OnMouseWheel(MapMouseWheelEventArgs e)
+        {
+            if ((e.WheelDelta > 0.0) && (this.ZoomLevel >= this.MapZoomRange.To))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnMouseWheel(e);
+            }
+        }
+        ///　<summary>
+        ///　重写鼠标拖放地图行为
+        ///　</summary>
+        ///　<param　name="e"></param>
+        public override void OnMouseDragBox(MapMouseDragEventArgs e)
+        {
+            if (((this.TargetBoundingRectangle.East <= this.LongitudeRange.To)
+                && (this.TargetBoundingRectangle.West >= this.LongitudeRange.From)))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+    }
+}
--- 03.需求分析/SCADA_Demo/SCADA.UI/Modules/BingMaps/MapIndex.xaml.cs
+++ 03.需求分析/SCADA_Demo/SCADA.UI/Modules/BingMaps/MapIndex.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl;
+using Microsoft.Maps.MapControl.Navigation;
 
 namespace SCADA.UI.Modules.BingMaps
 {
@@ -17,6 +19,60 @@ namespace SCADA.UI.Modules.BingMaps
         public MapIndex()
         {
             InitializeComponent();
+            //自定义导航
+            map.MapForeground.TemplateApplied += delegate(object sender, EventArgs args)
+            {
+                map.MapForeground.NavigationBar.TemplateApplied += delegate(object obj, EventArgs e)
+                {
+                    //清除导航菜单上现有项
+                    NavigationBar navBar = map.MapForeground.NavigationBar;
+                    navBar.HorizontalPanel.Children.Clear();
+                    //navBar.VerticalPanel.Children.Clear();
+                    //添加自定义导航菜单项
+
+                    ChangeMapModeButton btnRoad = new ChangeMapModeButton(new RoadMode(), "路况模式", "点击导航到路况模式");
+                    btnRoad.IsChecked = false;
+                    navBar.HorizontalPanel.Children.Add(btnRoad);
+                    ChangeMapModeButton btnAerial = new ChangeMapModeButton(new AerialMode(true), "卫星模式", "点击导航到卫星模式");
+                    btnAerial.IsChecked = true;
+                    navBar.HorizontalPanel.Children.Add(btnAerial);
+
+                    //分割线
+                    //navBar.HorizontalPanel.Children.Add(new CommandSeparator());
+                    //CommandToggleButton btnCQ = new CommandToggleButton(new CustomCommand("重庆"), "重庆", "地图定位到重庆");
+                    //navBar.HorizontalPanel.Children.Add(btnCQ);
+                    //Microsoft.Maps.MapControl.Navigation.ZoomSlider sliZool = new ZoomSlider();
+
+                    //navBar.HorizontalPanel .Children.Add(sliZool);
+                };
+            };
+
+            //将鸟瞰图模式添加到地图导航菜单
+            //BirdseyeMode.AddModeToNavigationBar(map);
+            //将街道模式添加到地图导航菜单(对中国地区支持的不好)
+            //StreetsideMode.AddModeToNavigationBar(map);
+
+            //初始化中文地图模式
+            ChinaMode chinaMode = new ChinaMode();
+            MapTileLayer tileChinaLayer = chinaMode.GetChinaTileLayer();
+            map.Children.Add(tileChinaLayer);
+            this.map.Mode = chinaMode;
+
+            map.ZoomLevel = 4;
+
+            //使中文地图适应模式的切换
+            map.ModeChanged += delegate(object sender, Microsoft.Maps.MapControl.MapEventArgs e)
+            {
+                if (map.Mode.GetType().Name != "RoadMode")
+                {
+                    tileChinaLayer.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    this.map.Mode = chinaMode;
+                    tileChinaLayer.Visibility = Visibility.Visible;
+                }
+            };
         }
     }
 }

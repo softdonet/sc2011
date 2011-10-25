0fc3a6bf112b5a2567e1b40afdf4373e3bf5a197
liuzhengzhan@hotmail.com <liuzhengzhan@hotmail.com@50ab2c4e-6467-85dd-b4b9-2c0a0049dd9d>
2011-10-25 16:15:46 +0000

    增加告警的“确认信息”

--- trunk/03.需求分析/SCADA_Demo/SCADA.UI/Modules/Alarm/AlarmList.xaml.cs
+++ trunk/03.需求分析/SCADA_Demo/SCADA.UI/Modules/Alarm/AlarmList.xaml.cs
@@ -17,6 +17,40 @@ namespace SCADA.UI.Modules.Alarm
         public AlarmList()
         {
             InitializeComponent();
+            MyContent.CloseBtn += new EventHandler(MyContent_CloseBtn);
+
         }
+
+        void MyContent_CloseBtn(object sender, EventArgs e)
+        {
+            Storyboard2.Begin();
+            ViewHost.Visibility = Visibility.Collapsed;
+        }
+
+        private void dgrdList_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.MouseLeftButtonUp -= new System.Windows.Input.MouseButtonEventHandler(Row_MouseLeftButtonUp);
+            e.Row.MouseLeftButtonUp += new System.Windows.Input.MouseButtonEventHandler(Row_MouseLeftButtonUp);
+        }
+
+        void Row_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //foreach (object ovj in dgrdList.ItemsSource)
+            //{
+            //    string aa = ((TextBox)dgrdList.Columns[1].GetCellContent(ovj)).Text;
+
+            //}
+            //string sColumnValue = ((TextBlock)this.dgrdList.Columns[1].GetCellContent(this.dgrdList.SelectedItem)).Text.Trim();
+
+            string columnValue = ((Expression.Blend.SampleData.DataGridViewSampleDataSource.Item)(this.dgrdList.SelectedItem)).DealWith;
+            if (columnValue == "确认")
+            {
+                Storyboard1.Begin();
+                MyContent.Content = new ConfirmPage();
+                MyContent.Title = "确认信息";
+            }
+
+        }
+
     }
 }
---
+++ trunk/03.需求分析/SCADA_Demo/SCADA.UI/Modules/Alarm/ConfirmPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace SCADA.UI.Modules.Alarm
+{
+    public partial class ConfirmPage : Page
+    {
+        public ConfirmPage()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}

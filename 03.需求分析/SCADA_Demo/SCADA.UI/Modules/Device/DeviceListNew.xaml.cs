83e417c9657e8f8dced9c0d40b00563dac59f380
yanghongkang <yanghongkang@gmail.com>
2011-10-29 10:42:54 +0000

    设备列表OK

--- 03.需求分析/SCADA_Demo/SCADA.UI/Modules/Device/DeviceListNew.xaml.cs
+++ 03.需求分析/SCADA_Demo/SCADA.UI/Modules/Device/DeviceListNew.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Data;
 using System.Globalization;
+using System.Windows.Media.Imaging;
+using System.Reflection;
 
 namespace SCADA.UI.Modules.Device
 {
@@ -19,7 +21,24 @@ namespace SCADA.UI.Modules.Device
         public DeviceListNew()
         {
             InitializeComponent();
-            
+            MyContent.CloseBtn += new EventHandler(MyContent_CloseBtn);
+        }
+        void MyContent_CloseBtn(object sender, EventArgs e)
+        {
+            Storyboard2.Begin();
+            ViewHost.Visibility = Visibility.Collapsed;
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.RadTreeListView1.ExpandAllHierarchyItems();
+        }
+
+
+        private void RadTreeListView1_RowActivated_1(object sender, Telerik.Windows.Controls.GridView.RowEventArgs e)
+        {
+            Storyboard1.Begin();
+            MyContent.Content = new DetailsPage();
+            MyContent.Title = "设备详细信息";
         }
     }
 
@@ -27,21 +46,23 @@ namespace SCADA.UI.Modules.Device
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            CoverageItemBase currentValue = value as CoverageItemBase;
-            if (value == null)
-            {
-                return DependencyProperty.UnsetValue;
-            }
-           
+            return "查看";
 
-            bool showCovered = Int16.Parse(parameter.ToString()) > 0;
+            //CoverageItemBase currentValue = value as CoverageItemBase;
+            //if (value == null)
+            //{
+            //    return DependencyProperty.UnsetValue;
+            //}
+            //return currentValue.BlocksCovered;
 
-            int relativeValue = (showCovered) ? currentValue.BlocksCovered : currentValue.BlocksNotCovered;
-            int totalValue = currentValue.BlocksCovered + currentValue.BlocksNotCovered;
+            //bool showCovered = Int16.Parse(parameter.ToString()) > 0;
 
-            if (totalValue == 0) return "0 %";
+            //int relativeValue = (showCovered) ? currentValue.BlocksCovered : currentValue.BlocksNotCovered;
+            //int totalValue = currentValue.BlocksCovered + currentValue.BlocksNotCovered;
 
-            return String.Format("{0:f2} %", (double)relativeValue / totalValue * 100);
+            //if (totalValue == 0) return "0 %";
+
+            //return String.Format("{0:f2} %", (double)relativeValue / totalValue * 100);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -55,7 +76,74 @@ namespace SCADA.UI.Modules.Device
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return String.Format("Images/{0}.png", value.GetType().Name);
+            int type = System.Convert.ToInt32(parameter);
+            Random rd = new Random();
+
+            int i = rd.Next(1, 10);
+            string imgState = string.Empty;
+            string imgElectric = string.Empty;
+            string imgSignal = string.Empty;
+            string img = string.Empty;
+            switch (i)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 8:
+                case 9:
+                case 10:
+                    switch (type)
+                    {
+                        case 1:
+                            img = "green.png";
+                            break;
+                        case 2:
+                            img = "electric4.png";
+                            break;
+                        case 3:
+                            img = "signal3.png";
+                            break;
+                    }
+                   
+                    break;
+                case 5:
+                    switch (type)
+                    {
+                        case 1:
+                            img = "red.png";
+                            break;
+                        case 2:
+                            img = "electric3.png";
+                            break;
+                        case 3:
+                            img = "signal2.png";
+                            break;
+                    }
+                    break;
+                case 6:
+                case 7:
+                    switch (type)
+                    {
+                        case 1:
+                            img = "grayer.png";
+                            break;
+                        case 2:
+                            img = "electric1.png";
+                            break;
+                        case 3:
+                            img = "signal1.png";
+                            break;
+                    }
+                  
+                    break;
+
+            }
+            AssemblyName assemblyName = new AssemblyName(typeof(RadTreeListXmlDataSource).Assembly.FullName);
+            string resourcePath = "/" + assemblyName.Name + ";component/Modules/Device/Image/" + img;
+            Uri resourceUri = new Uri(resourcePath, UriKind.Relative);
+            var obj = new BitmapImage(resourceUri);
+            return obj;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

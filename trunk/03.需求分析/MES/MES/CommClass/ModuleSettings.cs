4aae08764aa5b9a1f2873259562a7f2cd6827b43
yanghongkang@gmail.com <yanghongkang@gmail.com@50ab2c4e-6467-85dd-b4b9-2c0a0049dd9d>
2011-11-21 16:15:23 +0000

    串口扫描程序

--- trunk/03.需求分析/MES/MES/CommClass/ModuleSettings.cs
+++ trunk/03.需求分析/MES/MES/CommClass/ModuleSettings.cs
@@ -25,6 +25,22 @@ namespace MES.CommClass
             get { return _baudRate; }
             set { _baudRate = value; }
         }
+
+        private int _barCodeStart;
+        [DescriptionAttribute("条形码开始字符位置（序号从1开始）。"), CategoryAttribute("扫描枪通讯设置")]
+        public int BarCodeStart
+        {
+            get { return _barCodeStart; }
+            set { _barCodeStart = value; }
+        }
+
+        private int _barCodeLength;
+        [DescriptionAttribute("条形码长度。"), CategoryAttribute("扫描枪通讯设置")]
+        public int BarCodeLength
+        {
+            get { return _barCodeLength; }
+            set { _barCodeLength = value; }
+        }
     }
 
     #endregion 

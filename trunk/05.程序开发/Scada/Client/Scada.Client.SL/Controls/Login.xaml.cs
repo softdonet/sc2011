3d37903006081540e760bac107bc976dce9a7c91
yanghongkang@gmail.com <yanghongkang@gmail.com@50ab2c4e-6467-85dd-b4b9-2c0a0049dd9d>
2011-12-03 16:31:10 +0000

    局部迁移

---
+++ trunk/05.程序开发/Scada/Client/Scada.Client.SL/Controls/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+namespace Scada.Client.SL.Controls
+{
+    public partial class Login : UserControl
+    {
+        public event RoutedEventHandler myKeyDownEvent;
+        public Login()
+        {
+            InitializeComponent();
+            txtTile.Text = "CiC-T2000智能室温监测系统";
+            this.Loaded += new RoutedEventHandler(Login_Loaded);
+        }
+
+        void Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            HtmlPage.Plugin.Focus();
+            txbName.Focus();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (myKeyDownEvent != null)
+            {
+                this.myKeyDownEvent(this, new RoutedEventArgs());
+            }
+        }
+
+        private void UserControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (myKeyDownEvent != null)
+                {
+                    this.myKeyDownEvent(this, new RoutedEventArgs());
+                }
+            }
+        }
+    }
+}

c2b61e1b2a93e4f12a8c37be77dc3521ed5b5db4
liuzhengzhan@hotmail.com <liuzhengzhan@hotmail.com@50ab2c4e-6467-85dd-b4b9-2c0a0049dd9d>
2012-04-01 18:45:36 +0000

    告警 用户事件 查询基本完成

--- trunk/05.程序开发/Scada/Model/Scada.Model.Entity/DBPartialClass/UserEventModelPartial.cs
+++ trunk/05.程序开发/Scada/Model/Scada.Model.Entity/DBPartialClass/UserEventModelPartial.cs
@@ -8,5 +8,10 @@ namespace Scada.Model.Entity
     public partial class UserEventModel
     {
         public string EventTypeName { get; set; }
+        public string Step1 { get; set; }
+        public string Step2 { get; set; }
+        public string Step3 { get; set; }
+        public string Step4 { get; set; }
+        public string Step5 { get; set; }
     }
 }

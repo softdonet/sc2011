7d174bfefe8a17d9fe7cc1b75b1884afa90bb016
yanghongkang <yanghongkang@gmail.com>
2012-04-08 16:45:07 +0000

    添加全屏模式

--- 05.程序开发/Scada/Client/Scada.Client.SL/Controls/Header.xaml.cs
+++ 05.程序开发/Scada/Client/Scada.Client.SL/Controls/Header.xaml.cs
@@ -17,7 +17,37 @@ namespace Scada.Client.SL.Controls
         public Header()
         {
             InitializeComponent();
-          
+            Application.Current.Host.Content.FullScreenChanged += new EventHandler(Content_FullScreenChanged);
+        }
+
+        void Content_FullScreenChanged(object sender, EventArgs e)
+        {
+            if (Application.Current.Host.Content.IsFullScreen)
+            {
+                btnFull.Content = "退出全屏";
+                ToolTipService.SetToolTip(btnFull, "点击退出全屏");
+            }
+            else
+            {
+                btnFull.Content = "全屏模式";
+                ToolTipService.SetToolTip(btnFull, "点击放大到全屏");
+            }
+        }
+
+        private void btnFull_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Host.Content.IsFullScreen)
+            {
+                Application.Current.Host.Content.IsFullScreen = false;
+                btnFull.Content = "全屏模式";
+                ToolTipService.SetToolTip(btnFull, "点击放大到全屏");
+            }
+            else
+            {
+                Application.Current.Host.Content.IsFullScreen = true;
+                btnFull.Content = "退出全屏";
+                ToolTipService.SetToolTip(btnFull, "点击退出全屏");
+            }
         }
     }
 }

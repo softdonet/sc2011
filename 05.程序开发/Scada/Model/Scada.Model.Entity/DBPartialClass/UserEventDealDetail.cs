8f8c8ecaf983f23498ae140cc0005826bdef236a
liuzhengzhan <liuzhengzhan@hotmail.com>
2012-05-19 03:05:56 +0000

    用户事件和用户管理起来，显示当前操作步骤的用户

---
+++ 05.程序开发/Scada/Model/Scada.Model.Entity/DBPartialClass/UserEventDealDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scada.Model.Entity
+{
+    public partial class UserEventDealDetail
+    {
+        public string OperatorLoginID { get; set; }
+        public string OperatorLoginName { get; set; }
+    }
+}

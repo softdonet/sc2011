e3d451d22fa3e3ff63f4d0d671831e02518a9273
yanghongkang@gmail.com <yanghongkang@gmail.com@50ab2c4e-6467-85dd-b4b9-2c0a0049dd9d>
2012-07-08 02:22:09 +0000

    设备序列号解析、手机号码位数验证

--- trunk/05.程序开发/DataCollecting/DataCollecting/frmTool.cs
+++ trunk/05.程序开发/DataCollecting/DataCollecting/frmTool.cs
@@ -42,7 +42,7 @@ namespace DataCollecting
             h.CmdHeader = Const.UP_HEADER;
             h.CmdCommand = Command.cmd_RealTimeDate_R;
             h.DataContext = 42605;
-            h.DeviceSN = "0A5F01CD0003";
+            h.DeviceSN = "370201CD0100";
             h.State = 0;
             h.SateTimeMark = DateTime.Now;
             RealTimeData_R rr = new RealTimeData_R();
@@ -102,7 +102,7 @@ namespace DataCollecting
             h.CmdHeader = Const.UP_HEADER;
             h.CmdCommand = Command.cmd_UserEvent_R;
             h.DataContext = 42605;
-            h.DeviceSN = "0A5F01CD0001";
+            h.DeviceSN = "370201CD0102";
             h.State = 0;
             h.SateTimeMark = DateTime.Now;
 
